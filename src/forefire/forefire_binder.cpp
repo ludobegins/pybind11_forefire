84da499d76c374f3575a2e96e44f18abf4f2cadb
ludobegins <ludo.beghin@gmail.com>
2022-11-22 15:01:06 -0300

    restruct repo

--- src/forefire/forefire_binder.cpp
+++ src/forefire/forefire_binder.cpp
@@ -1,27 +1,14 @@
 #include <pybind11/pybind11.h>
 
-// #include "mymath.hpp"
+#include <../../lib/forefire/firefront/swig/PLibForeFire.h>
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(forefire, m) {
 
+    m.doc() = "Forefire simulation";
+
+    py::class_<PLibForeFire>(m, "PLibForeFire")
+        .def("getInst", &PLibForeFire::getInt);
+
 }
-
-
-
-    // m.doc() = "Forefire simulation";
-
-    // m.def("add", &add, "pbdoc(
-    //     Add two numbers
-
-    //     Some other explanation about the add function.
-    // )pbdoc");
-
-    // m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-    //     Subtract two numbers
-
-    //     Some other explanation about the subtract function.
-    // )pbdoc");
-
-    // m.attr("__version__") = "0.0.1";
